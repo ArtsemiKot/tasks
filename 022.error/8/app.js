f7ddc42c568d4af5e5c2df9997acd183b0ffb9b8
ArtsemiKot <kot_artemiy@mail.ru>
2023-08-12 19:25:54 +0300

    task-023

--- 022.error/8/app.js
+++ 022.error/8/app.js
@@ -3,3 +3,14 @@
 // ["a","b","c","d","f"] -> "e"
 // ["O","Q","R","S"] -> "P
 
+const alfa = 'abcdefghikjlmnoprstuvwxyz';
+const inpStr = ["O", "Q", "R", "S"].join('').toLowerCase();
+const indexStartInput = alfa.indexOf(inpStr[0]);
+const sliceFromAlfa = alfa.slice(indexStartInput,indexStartInput + inpStr.length + 1);
+
+for (let i = 0; i < sliceFromAlfa.length; i++) {
+    if (sliceFromAlfa[i] !== inpStr[i]) {
+        console.log(sliceFromAlfa[i]);
+        break;
+    }
+}
