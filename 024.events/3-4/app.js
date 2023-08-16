24c34c238e57c3e06f474db571991373779bceca
ArtsemiKot <kot_artemiy@mail.ru>
2023-08-16 23:46:33 +0300

    task-024

---
+++ 024.events/3-4/app.js
@@ -0,0 +1,17 @@
+// Необходимо отобразить кнопку с надписью «Нажми на меня» и пустой инпут. По клику на кнопку вызвать
+// alert и отобразить сообщение из значения инпута
+
+const btn = document.querySelector('button');
+
+function isValid(inp) {
+    if (!inp.value) throw new Error('Ошибка');
+}
+btn.addEventListener('click', function () {
+    try {
+        const inp = document.querySelector('input')
+        return isValid(inp);
+        alert(inp.value)
+    } catch (error) {
+        alert(error.message)
+    }
+})
