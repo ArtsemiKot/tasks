9ae5b28d4a3c54d10d8b264f2b63f18877bdd9ae
ArtsemiKot <kot_artemiy@mail.ru>
2023-10-19 19:57:59 +0300

    server

--- 035.server/index.js
+++ 035.server/index.js
@@ -12,9 +12,41 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello');
-});
+    const arr = [1, 2, 2, 2, 3, 4, 5, 5, 6, 2];
+    const new_arr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!new_arr.includes(arr[i])) {
+            new_arr.push(arr[i])
+        }
+    }
+    response.send(new_arr)
+})
+
+app.get('/:id', (request, response) => {
+    const { id } = request.params;
+    const arr = [1, 2, 3, 4, 5, 6];
+    const new_arr = [];
+    let item = [];
+    for (let i = 0; i < arr.length; i++) {
+        item.push(arr[i]);
+        if (item.length == id) {
+            new_arr.push(item);
+            item = []
+        }
+    }
+    item.length === 0 ? null : new_arr.push(item);
+    response.send(new_arr);
+})
+
+
+app.get('/about', (reques, response) => {
+    response.send('Hello world!')
+})
+
+app.get('/contact', (reques, response) => {
+    response.send('Hello!')
+})
 
 app.listen(3000, ()=>{
-    console.log('Этот порт 3000');
+    console.log('Этот север запущен на порт 3000');
 });
