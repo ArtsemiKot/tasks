f5e9ab8087827012435d5fa323e397722ac11b39
ArtsemiKot <kot_artemiy@mail.ru>
2023-12-03 10:20:28 +0300

    tasks

---
+++ 017.arr_methods/22/app.js
@@ -0,0 +1,15 @@
+// На входе n – количество элементов массива. Далее производится заполнение
+// массива с клавиатуры. Оставьте в нем только положительные числа. Filter
+
+const n = prompt();
+let arr = [];
+
+for (let i = 0; i < n; i++) {
+  let value = +prompt();
+  arr.push(value);
+}
+
+const new_arr=arr.filter(function(el){
+    return el%2===0?true:false
+})
+console.log(new_arr);
