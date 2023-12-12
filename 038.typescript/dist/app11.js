6aabc98efbf9575f342a24ddb1a097ed70eb2d69
ArtsemiKot <kot_artemiy@mail.ru>
2023-12-12 23:30:20 +0300

    tasks

--- 038.typescript/dist/app11.js
+++
@@ -1,5 +0,0 @@
-//11.Создайте переменную str и присвойте ей строковое значение. Используя цикл for выведите каждый символ строки и его индекс.
-const string_value = "hello";
-for (let i = 0; i < string_value.length; i++) {
-    console.log(string_value[i] + " " + i);
-}
--- 038.typescript/dist/app9.js
+++
@@ -1,9 +0,0 @@
-// 9. Напишите программу, которая принимает строку и возвращает новую строку, в
-// которой все символы уникальны (не повторяются). Например, для строки "hello"
-// результатом должна быть строка "helo".
-const string = "hello";
-let result = '';
-for (let i = 0; i < string.length; i++) {
-    result.includes(string[i]) ? null : result += string[i];
-}
-console.log(result);
