352e76f9918cf68e574061e2395dc8eae53724b5
ArtsemiKot <kot_artemiy@mail.ru>
2023-12-18 23:49:03 +0300

    tasks

---
+++ 040.typescript-function/src/f-app9.ts
@@ -0,0 +1,28 @@
+//Напишите код, который сделает из массива объект [ {name: 'width', value: 10}, {name: 'height', value: 20} ] -> {width: 10, height: 20}
+
+interface iUser9 {
+    name: string;
+    value: number;
+  }
+  
+  const arr9: iUser9[] = [
+    { name: "width", value: 10 },
+    { name: "height", value: 20 },
+  ];
+  
+  function test(arr9) {
+    interface iUserObj9 {
+      width: number;
+      height: number;
+    }
+  
+    let obj9 = {} as iUserObj9;
+  
+    for (let i = 0; i < arr9.length; i++) {
+      obj9[arr9[i].name] = arr9[i].value;
+    }
+  
+    return obj9;
+  }
+  
+  console.log(test(arr9));
